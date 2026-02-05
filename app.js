040ae24e76e8142fbffcc8bd66f225537fcb4ba8
3bdoo <89553459+AbdAlrhmanGit@users.noreply.github.com>
2026-02-05 03:41:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ let header = document.querySelector("header");
 let vh = document.scrollingElement.clientHeight;
 let scrollHeight = document.scrollingElement.scrollHeight;
 window.onscroll = function () {
-  if (window.scrollY >= vh && window.scrollY < scrollHeight - vh) {
+  if (Math.ceil(window.scrollY) >= vh && Math.ceil(window.scrollY) < scrollHeight - vh) {
     header.style.backgroundColor = "white";
   } else {
     header.style.backgroundColor = "transparent";
   }
 };
-
