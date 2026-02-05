2ae0409548788d0fac5501709c86a1d3088023db
3bdoo <89553459+AbdAlrhmanGit@users.noreply.github.com>
2026-02-05 03:15:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-let header = document.querySelector("header")
-let vh = document.scrollingElement.clientHeight
-let scrollHeight = document.scrollingElement.scrollHeight
-window.onscroll = function() {
-    if (window.scrollY >= vh && window.scrollY <= (scrollHeight - vh)){
-        header.style.backgroundColor = "white"
-    }else{
-        header.style.backgroundColor = "transparent"
-    }
-}
+let header = document.querySelector("header");
+let vh = document.scrollingElement.clientHeight;
+let scrollHeight = document.scrollingElement.scrollHeight;
+window.onscroll = function () {
+  if (window.scrollY >= vh && window.scrollY <= scrollHeight - vh) {
+    header.style.backgroundColor = "white";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+};
